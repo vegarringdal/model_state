4d234fae53e05523195ec8da79321407b482ef35
Vegar Ringdal <vegar.ringdal@gmail.com>
2024-03-07 23:24:59 +0100

    save

--- src/interface/model_store/utils/mesh.rs
+++ src/interface/model_store/utils/mesh.rs
@@ -9,5 +9,22 @@ struct Mesh {
     position_id: String,
     normal_id: Option<String>,
     texture_id: Option<String>,
-    
+
+
+    item_id: Vec<u32>,
+
+    item_material_id: Vec<u32>,
+    item_material_default_id: Vec<u32>,
+    item_material_override_id: Vec<u32>,
+
+    item_start: Vec<u32>,
+    item_count: Vec<u32>,
+
+    item_hidden: Vec<bool>,
+    item_transform_id: Vec<u32>,
+    item_bounding_boxes: Vec<Box>,
+
+    materials: HashMap<u32, Color, fnv_hasher::U32Hasher>,
+    transforms: HashMap<u32, Option<TransfomationMatrix>, fnv_hasher::U32Hasher>,
+
 }
